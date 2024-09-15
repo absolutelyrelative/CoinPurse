dfcae090af76800939b6a098f8bdce888d885265
Paolo <mrluigi35000vr@gmail.com>
2024-09-15 16:55:54 +0200

    Fixed SpringSecurity not allowing registration forms. Implemented password hashing.

--- src/main/java/com/coinpurse/web/security/SecurityConfig.java
+++ src/main/java/com/coinpurse/web/security/SecurityConfig.java
@@ -1,19 +1,35 @@
 package com.coinpurse.web.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers("/login", "/register", "/purses", "/css/**", "/js/**")
+        http.csrf().disable().authorizeHttpRequests().requestMatchers("/login", "/register", "/purses", "/css/**", "/js/**", "/purses/**", "/register/**")
                 .permitAll()
                 .and()
                 .formLogin(form -> form
@@ -27,5 +43,9 @@ public class SecurityConfig {
                         //        .logoutSuccessUrl("/logout").permitAll())
                 );
         return http.build();
+    }
+
+    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
