ba1c8d7bdeffb79ab43f550d57a5dfd70ce61f49
absolutely_relative <pawel.dunskimoc@gmail.com>
2026-09-09 22:07:59 +0200

    Bugfix: N-N CascadeType removal

--- src/main/java/com/coinpurse/web/model/UserEntity.java
+++ src/main/java/com/coinpurse/web/model/UserEntity.java
@@ -22,7 +22,7 @@ public class UserEntity {
     private String email;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_roles",
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
